d447984e774563f8819c2455799448ef3d4f7915
nativerv <nativerv@protonmail.com>
2024-04-02 18:57:49 +0300

    fix: `xdg-document-portal` per-app `--bind`-ing in `nixpak` (after a LOT of debugging and reverse engineering)

--- package/nixpak-test/default.nix
+++ package/nixpak-test/default.nix
@@ -1,11 +1,26 @@
 { pkgs, mkNixPak, ... }: let
-  name = "bash";
+  name = "nixpak-test";
+  appId = "org.myself.${name}";
+  package = pkgs.buildEnv {
+    inherit name;
+    paths = with pkgs; [
+      coreutils
+      glib
+      util-linux
+      bash
+      (writeShellScriptBin "${name}" ''
+        exec ${pkgs.bash}/bin/bash
+      '')
+    ];
+    buildInputs = [ pkgs.bash ];
+  };
 
   sandboxed = mkNixPak {
     config = { sloth, ... }: {
 
       # the application to isolate
-      app.package = pkgs.bash;
+
+      app.package = package;
 
       # path to the executable to be wrapped
       # this is usually autodetected but
@@ -20,6 +35,10 @@
       dbus.policies = {
         #"org.freedesktop.DBus" = "talk";
         #"ca.desrt.dconf" = "talk";
+        "org.freedesktop.portal.Desktop" = "talk";
+
+        # allow sending notifications
+        "org.freedesktop.Notifications" = "talk";
       };
 
       # needs to be set for Flatpak emulation
@@ -27,11 +46,13 @@
       # where ${name} is generated from the drv name like:
       # hello -> Hello
       # my-app -> MyApp
-      flatpak.appId = "org.myself.BashApp";
+      flatpak.appId = appId;
 
       gpu.enable = false;
 
       bubblewrap = {
+        env.PATH = "${package}/bin";
+
         # Bind only paths that app needs
         bindEntireStore = true;
 
@@ -41,19 +62,14 @@
         # lists of paths to be mounted inside the sandbox
         # supports runtime resolution of environment variables
         # see "Sloth values" below
-        bind.rw = [
-          #(sloth.concat' sloth.homeDir "/Documents")
-          #(sloth.env "XDG_RUNTIME_DIR")
-          ## a nested list represents a src -> dest mapping
-          ## where src != dest
-          #[
-          #  (sloth.concat' sloth.homeDir "/.local/state/nixpak/${name}/config")
-          #  (sloth.concat' sloth.homeDir "/.config")
-          #]
+        bind.rw = with sloth; [
+          ## bind src to dest
+          [ (mkdir (concat [xdgStateHome "/sandboxes/${name}/home"])) homeDir ]
+          [ (mkdir "/tmp/sandboxes/${name}") "/tmp" ]
+          ## bind when src is dest
+          #"/path/to/dir"
         ];
-        bind.ro = [
-          #(sloth.concat' sloth.homeDir "/Downloads")
-        ];
+        bind.ro = [ ];
         bind.dev = [
           #"/dev/dri"
         ];

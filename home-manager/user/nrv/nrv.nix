1adc8e4e007239c1c91c4c7472042856a872f036
nativerv <nativerv@protonmail.com>
2024-07-12 09:18:30 +0300

    feat(home-manager/user/nrv): figure out declarative channels & `home-manager` session variables

--- home-manager/user/nrv/nrv.nix
+++ home-manager/user/nrv/nrv.nix
@@ -2,6 +2,7 @@
 # Use this to configure your home environment (acts as legacy ~/.config/home-manager/home.nix)
 {
   self,
+  flake,
   inputs,
   ...
 }:
@@ -21,6 +22,9 @@
     packages = with pkgs; [
       hello
     ];
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
   };
 
   # Add stuff for your user as you see fit:
@@ -29,11 +33,34 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
+  #wayland.windowManager.hyprland.enable = true;
   #programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.05";
+  # FIXME: If first set to "nix/path" and then changed to "nix/path/nixpkgs",
+  #        `home-manager switch` fails with error: ln: permission denied
+  # NOTE: The following are not ideal way of doing channels anyway.
+  #       See `nix.channels` for that below or wherever in this flake.
+  #xdg.configFile."nix/channels/nixpkgs".source = pkgs.nixpkgs-flake;
+  #nix.nixPath = [ "${config.xdg.configHome}/nix/channels" ];
+
+  # Setup declarative user channels / access to <nixpkgs> in
+  # legacy nix commands
+  nix.package = pkgs.nixVersions.latest;
+  #nix.settings = (pkgs.callPackage (import "${flake}/nixos/module/archetype/sane.nix" { inherit self flake inputs; }) {}).nix.settings;
+  nix.settings = {
+    use-xdg-base-directories = true;
+  };
+  nix.channels = {
+    nixpkgs = pkgs.nixpkgs-flake;
+  };
+
+  # Source home manager session variables in ZSH config
+  xdg.configFile."zsh/home-manager".source = pkgs.writeText "home-manager" ''
+    # \'\' is multi-line string escape for... reasons
+    . "''${XDG_STATE_HOME}/nix/profiles/home-manager/home-path/etc/profile.d/hm-session-vars.sh"
+    # vim:ft=zsh
+  '';
 }

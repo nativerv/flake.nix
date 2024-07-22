77e338e6690de66729ad6e4ae92167712bde0494
nativerv <nativerv@protonmail.com>
2024-07-22 17:39:51 +0300

    feat: `archetype.default`: custom groups; `user.nrv`: add to custom groups

---
+++ nixos/module/archetype/default.nix
@@ -0,0 +1,22 @@
+# This is not a 'default' module but an archetype named 'default'
+# It contains stuff that all my default systems should have.
+# For an unopinionated (not really) defaults to to sane.nix.
+{
+self,
+...
+}:
+{
+...
+}:
+{
+  imports = [
+    self.nixosModules."archetype.sane"
+  ];
+  # My custom groups under my own gid range. Defined in a common config file.
+  # NOTE: NixOS does not permit specifying extra groups as gids.
+  #       All of the places referencing my custom groups would refer to the
+  #       shared config instead of hard string names if it were not the case.
+  # TODO: maybe fix the NixOS gid thing in the future.
+  users.groups = self.config.groups;
+}
+
--- nixos/module/user/nrv.nix
+++ nixos/module/user/nrv.nix
@@ -1,16 +1,32 @@
-{ ... }:
+{
+  self,
+  ...
+}:
 { ... }:
 let
   name = "nrv";
+  id = 1337010000;
 in {
-  users.users = {
-    ${name} = {
-      initialPassword = "123";
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/EhBI6sJb2yHbTkqhZiCzUrsLE6t+CZe7RhS22z7w5"
-      ];
-      extraGroups = [ "wheel" ];
-    };
+  users.groups.${name} = {
+    gid = id;
+  };
+  users.users.${name} = {
+    uid = id;
+    group = "${name}";
+    extraGroups = [
+      "wheel"
+      "part"
+      "pool"
+      "media"
+      "vg"
+      "steam"
+    ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/EhBI6sJb2yHbTkqhZiCzUrsLE6t+CZe7RhS22z7w5"
+    ];
+
+    initialPassword = "123";
+    isNormalUser = true;
   };
 }

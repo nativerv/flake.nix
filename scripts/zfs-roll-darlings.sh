38cde95c37fd15c168af3ed0ccf21c03f956d336
nativerv <nativerv@protonmail.com>
2024-08-05 20:19:54 +0300

    feat: second take on script for rolling my darlings with ZFS

--- scripts/zfs-roll-darlings.sh
+++ scripts/zfs-roll-darlings.sh
@@ -5,21 +5,50 @@ set -u
 set +e
 
 max=10
+local_dataset="${pool}/sys/${system}/local"
+now_dataset="${local_dataset}/now"
+old_dataset="${local_dataset}/old"
+old_boots_dataset="${old_dataset}/boot"
 
-zfs destroy -r "${pool}/sys/${system}/local.boot.${max}"
+zfs create -up \
+  -o mountpoint="/old" \
+  -o canmount='noauto' \
+  "${old_dataset}"
 
+zfs create -up \
+  "${old_boots_dataset}"
+
+# "mythos/sys/adamantia/local/now"
+# "mythos/sys/adamantia/local/old/boot/01"
+# "mythos/sys/adamantia/local/old/boot/02"
+# "mythos/sys/adamantia/local/old/boot/03"
+
+# Roll out the oldest / dataset
+zfs destroy -r "${old_boots_dataset}/${max}"
+
+# Shift all the other old / datasets
 for n in $(seq 2 "${max}" | sort -rn); do
   n_prev="$(printf '%02d\n' "$((n-1))")"
   n="$(printf '%02d\n' "${n}")"
-  zfs rename "${pool}/sys/${system}/local.boot.${n_prev}" "${pool}/sys/${system}/local.boot.${n}" &&
-    zfs set mountpoint="/old/boot.${n}" "${pool}/sys/${system}/local.boot.${n}" &&
-    zfs set canmount='noauto' "${pool}/sys/${system}/local.boot.${n}" 
+  zfs rename -u "${old_boots_dataset}/${n_prev}" "${old_boots_dataset}/${n}"
 done
 
-zfs snapshot "${pool}/sys/${system}/local@boot"
-zfs rename "${pool}/sys/${system}/local" "${pool}/sys/${system}/local.boot.01"
-zfs set readonly='on' "${pool}/sys/${system}/local.boot.01"
-zfs set mountpoint="/old/boot.01" "${pool}/sys/${system}/local.boot.01"
-zfs set canmount='noauto' "${pool}/sys/${system}/local.boot.01" 
-zfs clone "${pool}/sys/${system}/local.boot.01@blank" "${pool}/sys/${system}/local"
-zfs promote "${pool}/sys/${system}/local"
+# Then, we first snapshot the state of current / dataset.
+zfs snapshot "${now_dataset}@boot"
+# Then move the current / to be boot/01 (older datasets were shifted by the
+# loop above - the name boot/01 is free)
+zfs rename -u "${now_dataset}" "${old_boots_dataset}/01"
+# Then set it readonly - because it's basically a snapshot and should never be
+# modified. The whole `clone` setup is made specifically because ZFS can't have
+# never snapshots than the one you rolling back to.
+zfs set readonly='on' "${old_boots_dataset}/01"
+# Reset mountpoint on the old one - so it can be inherited and auto-mounted.
+zfs inherit mountpoint "${old_boots_dataset}/01"
+zfs umount "${old_boots_dataset}/01"
+
+# Clone the @blank to be a new /
+zfs clone "${old_boots_dataset}/01@blank" "${now_dataset}"
+# Umount it so it doesn't get in the way
+
+# Promote it because why not
+zfs promote "${now_dataset}"

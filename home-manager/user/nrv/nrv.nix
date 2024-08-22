1cc3eda6b92fadb47d7ddeddba0ec31c12fa057b
nativerv <nativerv@protonmail.com>
2024-08-22 08:24:34 +0300

    fix(hm): `nrv`: -redundant .enable before it becomes a problem

--- home-manager/user/nrv/nrv.nix
+++ home-manager/user/nrv/nrv.nix
@@ -74,7 +74,6 @@ in {
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  #programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";

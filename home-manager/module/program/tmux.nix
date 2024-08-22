7246a801bd064700c579ab3b952e95c1bec65bb2
nativerv <nativerv@protonmail.com>
2024-08-22 08:15:11 +0300

    feat(hm): `program.tmux`: +resurrect (bring hash & pin)

--- home-manager/module/program/tmux.nix
+++ home-manager/module/program/tmux.nix
@@ -6,34 +6,26 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
   programs.tmux = {
     enable = true;
-    # This is all bullshit. Just reuse the file below
-    # mouse = true;
-    # prefix = "M-`";
-    # keyMode = "vi";
-    #
-    # # Prevent tmux from spawning login shells
-    # # set -g default-command "${SHELL}"
-    #
-    # # Remove delay when switching between Vim modes.
-    # escapeTime = 0;
-    #
-    # # Scrollback history
-    # historyLimit = 500000;
-    #
-    # # Enable color support inside of tmux
-    # # `xterm-256color` => no (colored/any) undercurles in `nvim`
-    # # `xterm-kitty`    => also works
-    # terminal = "tmux-256color";
 
-    # Enable undercurles support inside of tmux
-    # set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-    # Enable colored underlines (and undercurles) support inside of tmux
-    # set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.resurrect.overrideAttrs (old: {
+	  version = "6be2f34";
+	  src = fetchFromGitHub {
+	    inherit (old.src) owner repo;
+	    rev = "6be2f34b5f06c8a6a23dc3dc835e69c132d58a18";
+	    sha256 = "sha256-1NXk75eZbhLEq0KKpLaFegPj0xLvWnLrVVfPxK1mk18=";
+	  };
+	});
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+    ];
 
     # Import my old config for now - seems to work fine apart from plugins
     extraConfig = builtins.readFile "${flake}/home-manager/module/program/tmux/tmux.conf";

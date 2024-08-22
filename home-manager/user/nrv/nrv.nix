24c988a87dd7ff7f64eb7d2a4aa3fa1097e6ac7c
nativerv <nativerv@protonmail.com>
2024-08-22 08:39:48 +0300

    feat(hm): `nrv`: +`jq`, `ripgrep`, `sxiv`

--- home-manager/user/nrv/nrv.nix
+++ home-manager/user/nrv/nrv.nix
@@ -33,6 +33,9 @@ in {
       pulsemixer
       inputs.tmux-sessionizer.packages.${system}.default
       wl-clipboard
+      jq
+      ripgrep
+      sxiv
       (pkgs.wrapPackages [ pkgs.restic ] {
         environment = {
           RESTIC_PASSWORD_COMMAND = "${pass}/bin/pass show ${self.config.backups.restic.key.sk.pass-path or "dummy"}";

38e42a2199fc7a7c73e77e6a5cfc36a3e137d8d0
nativerv <nativerv@protonmail.com>
2024-08-31 03:29:50 +0300

    feat(package): +`hotkey-scripts`: bring

---
+++ package/hotkey-scripts/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, ... }: stdenv.mkDerivation {
+  name = "hotkey-scripts";
+  pname = "hotkey-scripts";
+  src = ./.;
+
+  # nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -Dm755 script/* -t $out/bin
+  '';
+
+  # postFixup = with pkgs; ''
+  #   for bin in $out/bin/*; do
+  #     wrapProgram $bin \
+  #       --suffix PATH : ${lib.makeBinPath [
+  #         coreutils
+  #         sudo
+  #         gawk
+  #         gnugrep
+  #         bash
+  #         procps
+  #       ]}
+  #   done
+  # '';
+}

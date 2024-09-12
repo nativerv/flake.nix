95e6217314e64d770a72d348901c996e9bd95f06
nativerv <nativerv@protonmail.com>
2024-09-12 10:40:44 +0300

    feat(nixos): `program.keyd`: re-enable super+` in `keyd`

--- home-manager/user/nrv/nrv.nix
+++ home-manager/user/nrv/nrv.nix
@@ -56,6 +56,7 @@ in {
       zip
       chafa
       pulseaudio
+      self.packages.${system}.scripts
       (pkgs.wrapPackages [ pkgs.restic ] {
         environment = {
           RESTIC_PASSWORD_COMMAND = "${pass}/bin/pass show ${self.config.backups.restic.key.sk.pass-path or "dummy"}";

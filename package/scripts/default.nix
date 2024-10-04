183924d9f83da3f5537d3fc643ba111b4d0af045
nativerv <nativerv@protonmail.com>
2024-10-04 06:51:05 +0300

    build(package): `scripts`: hardcoded: `xdg-doc-allow`: dep

--- package/scripts/default.nix
+++ package/scripts/default.nix
@@ -1,26 +1,21 @@
-{ stdenv, ... }: stdenv.mkDerivation {
+{ stdenv, pkgs, ... }: stdenv.mkDerivation {
   name = "dream-scripts";
   pname = "dream-scripts";
   src = ./.;
-
-  # nativeBuildInputs = with pkgs; [ makeWrapper ];
 
   installPhase = ''
     mkdir -p $out/bin
     install -Dm755 script/* -t $out/bin
   '';
 
-  # postFixup = with pkgs; ''
-  #   for bin in $out/bin/*; do
-  #     wrapProgram $bin \
-  #       --suffix PATH : ${lib.makeBinPath [
-  #         coreutils
-  #         sudo
-  #         gawk
-  #         gnugrep
-  #         bash
-  #         procps
-  #       ]}
-  #   done
-  # '';
+  nativeBuildInputs = with pkgs; [ makeWrapper ];
+  postFixup = with pkgs; /* bash */ ''
+    for bin in $out/bin/*; do
+      wrapProgram $bin \
+        --suffix PATH : ${lib.makeBinPath [
+          # FIXME(hardcoded): dep of one specific script
+          glib # for xdg-doc-allow
+        ]}
+    done
+  '';
 }

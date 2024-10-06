e0df27d20648ca8bd668fd75f663ef4d5b39c805
nativerv <nativerv@protonmail.com>
2024-10-06 11:15:48 +0300

    refactor(nixos): +`default`: `dream`

--- nixos/module/default.nix
+++ nixos/module/default.nix
@@ -8,11 +8,14 @@ with inputs.nixpkgs-24-05.lib;
 with self.lib;
 let
   # Combine dream modules into one
-  dream.imports = attrValues (readModulesRecursive'
-    ./dream
-    { inherit flake self inputs; });
+  dream.imports = attrValues (
+    readModulesRecursive' ./dream { inherit flake self inputs; }
+  );
   # Read generic modules seperately
   generic = readModulesRecursive'
     ./generic
     { inherit flake self inputs; };
-in generic // { inherit dream; }
+in generic // {
+  inherit dream;
+  default = dream;
+}

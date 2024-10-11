475e202a1dd6874a6e4e90e4700fa443fa38c3d3
nativerv <nativerv@protonmail.com>
2024-10-11 09:15:34 +0300

    chore(nixos): `subsystem.networking`: -dead comment

--- nixos/module/dream/subsystem/networking.nix
+++ nixos/module/dream/subsystem/networking.nix
@@ -8,20 +8,6 @@
   ...
 }:
 
-# Storing Linux 5.9 rc4
-# on a compressed zram block device 
-# ---------------------------------
-# Algorithm 	cp time 	Data 	Compressed 	Total
-# lzo         4.571s    1.1G  387.8M      409.8M
-# lzo-rle     4.471s    1.1G  388M        410M
-# lz4         4.467s    1.1G  403.4M      426.4M
-# lz4hc       14.584s   1.1G  362.8M      383.2M
-# 842         22.574s   1.1G  538.6M      570.5M
-# zstd        7.897s    1.1G  285.3M      298.8M 
-# ---------------------------------
-# Summary: lzo-rle --- best for speed
-#          zstd    --- best for size
-
 with builtins;
 with lib;
 with self.lib;

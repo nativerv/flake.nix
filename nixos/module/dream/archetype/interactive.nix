55e2ea57a3a00cbadc3744eb2e405034af1cbd78
nativerv <nativerv@protonmail.com>
2024-10-12 23:18:14 +0300

    feat(nixos): `archetype.interactive`: +sane expected utils

--- nixos/module/dream/archetype/interactive.nix
+++ nixos/module/dream/archetype/interactive.nix
@@ -16,8 +16,15 @@ let
 in
 {
   options.dream.archetype.interactive = {
-    enable = mkEnableOption "Enable archetype.interactive";
+    enable = mkEnableOption "Enable archetype.interactive - humas will log in to this machine";
   };
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      file
+      rsync
+      p7zip
+      curl
+    ];
+    dream.program.htop.enable = true;
   };
 }

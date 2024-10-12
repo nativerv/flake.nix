0cd6f562702cd3e6ca1f80928dea83060d94a01f
nativerv <nativerv@protonmail.com>
2024-10-12 22:25:37 +0300

    feat(nixos): `archetype.quick`: +terminal utils for interactive

--- nixos/module/dream/archetype/quick.nix
+++ nixos/module/dream/archetype/quick.nix
@@ -14,20 +14,34 @@ with self.lib;
 let
   cfg = config.dream.archetype.quick;
   cfgGraphical = config.dream.archetype.graphical;
+  cfgInteractive = config.dream.archetype.interactive;
+  inherit (pkgs) system;
 in
 {
   options.dream.archetype.quick = {
-    enable = mkEnableOption "Enable archetype.quick - quick setup that pulls wanted stuff globally depending on other archetypes";
+    enable = mkEnableOption "Enable archetype.quick - this machine is a quick setup that should have common (opinionated) programs installed, depending on other archetypes. Basically this archetype is a is a 'distro' of NixOS with sane defaults for use";
   };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; mkIf cfgGraphical.enable [
-      imv
-      kitty
-      self.packages.${system}.firefox
-      self.packages.${system}.telegram-desktop
-      self.packages.${system}.gimp
-      self.packages.${system}.mpv
-      self.packages.${system}.zathura
-    ];
+    environment.systemPackages = with pkgs;
+      (optionals cfgGraphical.enable [
+        imv
+        kitty
+        self.packages.${system}.firefox
+        self.packages.${system}.ungoogled-chromium
+        self.packages.${system}.telegram-desktop
+        self.packages.${system}.gimp
+        self.packages.${system}.mpv
+        self.packages.${system}.zathura
+      ]) ++
+      (optionals cfgInteractive.enable [
+        ncdu
+        htop-vim
+        ranger
+        restic
+        rclone
+        calc
+        chafa
+      ]);
+    # TODO: enable programs.*
   };
 }

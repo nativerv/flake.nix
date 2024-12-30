446bfe322e01cbf93ef241d3d82e2245b97503fe
nativerv <nativerv@protonmail.com>
2024-12-30 03:06:26 +0300

    feat(hm): `program.direnv`: ~silent, centralized cache

--- home-manager/module/dream/program/direnv.nix
+++ home-manager/module/dream/program/direnv.nix
@@ -24,7 +24,20 @@ in {
   config = mkIf cfg.enable {
     programs.direnv = {
       enable = true;
+      silent = true;
       nix-direnv.enable = true;
     };
+    xdg.configFile."direnv/direnvrc".text = /* bash */ ''
+      : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              hash="$(sha1sum - <<< "$PWD" | head -c40)"
+              path="''${PWD//[^a-zA-Z0-9]/-}"
+              echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+          )}"
+      }
+    '';
   };
 }

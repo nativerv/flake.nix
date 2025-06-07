97e2c211b3a32296c323b865f17b2e6a36a6e8b5
nativerv <nativerv@protonmail.com>
2025-06-08 02:58:18 +0300

    feat(hm): `program.tmux`: -`resurrect`

--- home-manager/module/dream/program/tmux.nix
+++ home-manager/module/dream/program/tmux.nix
@@ -24,17 +24,17 @@ in
       enable = true;
 
       plugins = with pkgs; [
-        {
-          plugin = tmuxPlugins.resurrect.overrideAttrs (old: {
-            version = "6be2f34";
-            src = fetchFromGitHub {
-              inherit (old.src) owner repo;
-              rev = "6be2f34b5f06c8a6a23dc3dc835e69c132d58a18";
-              sha256 = "sha256-1NXk75eZbhLEq0KKpLaFegPj0xLvWnLrVVfPxK1mk18=";
-            };
-          });
-          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-        }
+        # {
+        #   plugin = tmuxPlugins.resurrect.overrideAttrs (old: {
+        #     version = "6be2f34";
+        #     src = fetchFromGitHub {
+        #       inherit (old.src) owner repo;
+        #       rev = "6be2f34b5f06c8a6a23dc3dc835e69c132d58a18";
+        #       sha256 = "sha256-1NXk75eZbhLEq0KKpLaFegPj0xLvWnLrVVfPxK1mk18=";
+        #     };
+        #   });
+        #   extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        # }
       ];
 
       # Import my old config for now - seems to work fine apart from plugins
